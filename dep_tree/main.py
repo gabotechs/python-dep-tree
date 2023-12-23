b4fbd64d4d6c3620fcd5d5a81ae848f3897be208
gabotechs <gb.mt.me@gmail.com>
2023-12-23 12:15:30 +0000

    bump: version 0.15.5

--- dep_tree/main.py
+++ dep_tree/main.py
@@ -7,7 +7,7 @@ import os.path as path
 import tarfile
 import shutil
 
-__version__ = '0.15.4'
+__version__ = '0.15.5'
 
 BIN = path.join(path.dirname(__file__), 'dep-tree')
 BIN_TAR = BIN + '.tar.gz'
